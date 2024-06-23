d4f6ae6452ecfbe3c5ce427e4a7b05599a2c5fe5
thepriest0 <thereviewmr@gmail.com>
2024-06-23 20:10:05 +0100

    geh

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
-    UPLOAD_FOLDER = 'uploads'
-    ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'pdf', 'doc', 'docx'}
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
